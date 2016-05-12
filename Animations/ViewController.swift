24976ce6301f79681b195e5514461b7bcf5a3678
Sarah Olson <solson@nerdery.com>
2016-05-12 18:36:03 -0500

    Lesson 2: Red/blue squares

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -12,9 +12,30 @@ class ViewController: UIViewController {
 
     let transitionManager = TransitionManager()
     
+    let container = UIView()
+    let redSquare = UIView()
+    let blueSquare = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set container frame and add to the screen
+        self.container.frame = CGRect(x: 60, y: 60, width: 200, height: 200)
+        self.view.addSubview(container)
+        
+        // set red square frame up
+        // we want the blue square to have the same position as redSquare
+        // so lets just reuse blueSquare.frame
+        self.redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.blueSquare.frame = redSquare.frame
+        
+        // set background colors
+        self.redSquare.backgroundColor = UIColor.redColor()
+        self.blueSquare.backgroundColor = UIColor.blueColor()
+        
+        // for now just add the redSquare
+        // we'll add blueSquare as part of the transition animation
+        self.container.addSubview(self.redSquare)   
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -30,6 +51,25 @@ class ViewController: UIViewController {
     
     @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
         // empty for now
+    }
+    
+    @IBAction func animateButtonTapped(sender: AnyObject) {
+
+        // create a 'tuple' (a pair or more of objects assigned to a single variable)
+        var views : (frontView: UIView, backView: UIView)
+        
+        if((self.redSquare.superview) != nil){
+            views = (frontView: self.redSquare, backView: self.blueSquare)
+        }
+        else {
+            views = (frontView: self.blueSquare, backView: self.redSquare)
+        }
+        
+        // set a transition style
+        let transitionOptions = UIViewAnimationOptions.TransitionFlipFromBottom
+        
+        // with no animation block, and a completion block set to 'nil' this makes a single line of code
+        UIView.transitionFromView(views.frontView, toView: views.backView, duration: 1.0, options: transitionOptions, completion: nil)
     }
     
     // we override this method to manage what style status bar is shown
