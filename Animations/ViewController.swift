d763339ff022154100d18806333819553e097650
Sarah Olson <solson@nerdery.com>
2016-05-13 06:41:05 -0500

    Lesson 2: Animate drawing of a circle

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     let transitionManager = TransitionManager()
 
     override func viewDidLoad() {
-        super.viewDidLoad()        
+        super.viewDidLoad()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -33,42 +33,40 @@ class ViewController: UIViewController {
     
     @IBAction func animateButtonTapped(sender: AnyObject) {
 
-        // loop from 0 to 5
-        for i in 0...5 {
-            
-            // create a square
-            let square = UIView()
-            square.frame = CGRect(x: 55, y: 300, width: 20, height: 20)
-            square.backgroundColor = UIColor.redColor()
-            self.view.addSubview(square)
-            
-            // randomly create a value between 0.0 and 150.0
-            let randomYOffset = CGFloat( arc4random_uniform(150))
-            
-            // for every y-value on the bezier curve
-            // add our random y offset so that each individual animation
-            // will appear at a different y-position
-            let path = UIBezierPath()
-            path.moveToPoint(CGPoint(x: 16,y: 239 + randomYOffset))
-            path.addCurveToPoint(CGPoint(x: 301, y: 239 + randomYOffset), controlPoint1: CGPoint(x: 136, y: 373 + randomYOffset), controlPoint2: CGPoint(x: 178, y: 110 + randomYOffset))
-            
-            // create the animation
-            let anim = CAKeyframeAnimation(keyPath: "position")
-            anim.path = path.CGPath
-            anim.rotationMode = kCAAnimationRotateAuto
-            anim.repeatCount = Float.infinity
-            
-            // each square will take between 4.0 and 8.0 seconds
-            // to complete one animation loop
-            anim.duration = Double(arc4random_uniform(40)+30) / 10
-            
-            // stagger each animation by a random value
-            // `290` was chosen simply by experimentation
-            anim.timeOffset = Double(arc4random_uniform(290))
-            
-            // add the animation 
-            square.layer.addAnimation(anim, forKey: "animate position along path")
-        }
+        // set up some values to use in the curve
+        let ovalStartAngle = CGFloat(90.01 * M_PI/180)
+        let ovalEndAngle = CGFloat(90 * M_PI/180)
+        let ovalRect = CGRectMake(97.5, 58.5, 125, 125)
+        
+        // create the bezier path
+        let ovalPath = UIBezierPath()
+        
+        ovalPath.addArcWithCenter(CGPointMake(CGRectGetMidX(ovalRect), CGRectGetMidY(ovalRect)),
+                                  radius: CGRectGetWidth(ovalRect) / 2,
+                                  startAngle: ovalStartAngle,
+                                  endAngle: ovalEndAngle, clockwise: true)
+        
+        // create an object that represents how the curve
+        // should be presented on the screen
+        let progressLine = CAShapeLayer()
+        progressLine.path = ovalPath.CGPath
+        progressLine.strokeColor = UIColor.blueColor().CGColor
+        progressLine.fillColor = UIColor.clearColor().CGColor
+        progressLine.lineWidth = 10.0
+        progressLine.lineCap = kCALineCapRound
+        
+        // add the curve to the screen
+        self.view.layer.addSublayer(progressLine)
+        
+        // create a basic animation that animates the value 'strokeEnd'
+        // from 0.0 to 1.0 over 3.0 seconds
+        let animateStrokeEnd = CABasicAnimation(keyPath: "strokeEnd")
+        animateStrokeEnd.duration = 3.0
+        animateStrokeEnd.fromValue = 0.0
+        animateStrokeEnd.toValue = 1.0
+        
+        // add the animation
+        progressLine.addAnimation(animateStrokeEnd, forKey: "animate stroke end animation")
     }
     
     // we override this method to manage what style status bar is shown
